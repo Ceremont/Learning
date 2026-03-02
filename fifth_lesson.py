8b449d18011dbc4e87e91be19bd5e3fa9ffff46f
Daniel <dpavlicenko9@gmail.com>
2026-03-03 00:38:58 +0100

    fifth lesson. Functions

--- fifth_lesson.py
+++ fifth_lesson.py
@@ -21,9 +21,33 @@ def calculate(a, b, operation):
         return "Unknown operation"
 
 def run_calculator():
-    number1 = int(input("Enter a number: "))
-    number2 = int(input("Enter another number: "))
+    try:
+        number1 = float(input("Enter a number: "))
+        number2 = float(input("Enter another number: "))
+    except ValueError:
+        return "Invalid number"
+
     operation = input("Enter operation: ")
-    result = calculate(number1, number2, operation)
-    return result
+    return calculate(number1, number2, operation)
 print(run_calculator())
+def calculate_many(*numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+print(calculate_many(1, 2, 3))
+def power(number,exponent=2):
+    return number ** exponent
+print(power(2,15))
+def create_user(name, age, country="Unknown"):
+    return f"Name: {name}, Age: {age}, Country: {country}"
+print(create_user(age=25, name="Daniel", country="Poland"))
+def create_profile(**info):
+    for key, value in info.items():
+        print(f"{key}: {value}")
+def make_order(produkt,price,quantity=1,**extras):
+    total = price * quantity
+    for value in extras:
+        total += extras[value]
+    return total
+print(make_order("Phone", 500, quantity=2, delivery=20, case=10))
